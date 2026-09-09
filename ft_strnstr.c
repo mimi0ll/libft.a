a58182862301028123b982a9d457e41055f6925d
mimi0ll <mhatamleh507@gmail.com>
2026-09-09 14:19:29 +0300

    Fix ft_strnstr to handle empty needle case

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,18 +3,18 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
     size_t i;
     size_t j;
 
-    i = 0;
-    while (!*needle)
+    if (*needle == '\0')
         return ((char *)haystack);
-    while(haystack[i] && i < len)
+
+    i = 0;
+    while (haystack[i] && i < len)
     {
         j = 0;
-        while (haystack[i + j] == needle[j] && (i + j) < len)
-        {
-            if (needle[j + 1] == '\0')
-                return ((char *)(haystack + i));
+        while (needle[j] && (i + j) < len
+            && haystack[i + j] == needle[j])
             j++;
-        }
+        if (needle[j] == '\0')
+            return ((char *)(haystack + i));
         i++;
     }
     return (NULL);
