771888358b8290221365f00d2f8a28c2f72a53a1
mimi0ll <mhatamleh507@gmail.com>
2026-09-13 08:37:01 +0300

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -106,3 +106,116 @@ char    **ft_split(char const *s, char c)
     result[i] = NULL;
     return (result);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+```c
+#include "libft.h"
+
+static int	count_words(char const *s, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			count++;
+			while (*s && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	return (count);
+}
+
+static void	copy_word(char *dest, char const *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+static void	free_result(char **result, size_t i)
+{
+	size_t	j;
+
+	j = 0;
+	while (j < i)
+	{
+		free(result[j]);
+		j++;
+	}
+	free(result);
+}
+
+static int	fill_word(char **result, char const **s, size_t i, char c)
+{
+	size_t	len;
+
+	while (**s == c)
+		(*s)++;
+	len = 0;
+	while ((*s)[len] && (*s)[len] != c)
+		len++;
+	result[i] = malloc(sizeof(char) * (len + 1));
+	if (!result[i])
+	{
+		free_result(result, i);
+		return (0);
+	}
+	copy_word(result[i], *s, len);
+	*s += len;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+	size_t	i;
+	int		count;
+
+	if (!s)
+		return (NULL);
+	count = count_words(s, c);
+	result = malloc(sizeof(char *) * (count + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < (size_t)count)
+	{
+		if (!fill_word(result, &s, i, c))
+			return (NULL);
+		i++;
+	}
+	result[i] = NULL;
+	return (result);
+}
+```
+
+
+
+
+
+
+
+
